784ca9f0a262b285ecc7948e0ed3bacd5a51142d
RamadanismaeL <ramadan.ismael02@gmail.com>
2024-09-11 15:29:08 +0200

    Dependencia resolvida

--- ram/src/main/java/com/jmc/mazebank/App.java
+++ ram/src/main/java/com/jmc/mazebank/App.java
@@ -4,26 +4,35 @@
 
 package com.jmc.mazebank;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class App extends Application {
 
     @SuppressWarnings("exports")
     @Override
-    public void start(Stage stage) throws Exception {
-        /*FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());*/
+    public void start(Stage stage) {        
+        try {
+            /*FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());*/
 
-        Parent root = FXMLLoader.load(getClass().getResource("/com/jmc/mazebank/Fxml/Login.fxml"));
-        Scene scene = new Scene(root);
-        stage.initStyle(StageStyle.TRANSPARENT);
-        stage.setScene(scene);
-        stage.show();
+            Parent root = FXMLLoader.load(getClass().getResource("/com/jmc/mazebank/Fxml/text.fxml"));
+            Scene scene = new Scene(root);
+            //stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("Erro ao carregar recursos: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Erro inesperado: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
