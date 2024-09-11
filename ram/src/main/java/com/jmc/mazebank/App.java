f29f59ad9914af38874b2be254e63f3aa1ee6400
RamadanismaeL <ramadan.ismael02@gmail.com>
2024-09-11 14:04:18 +0200

    Trabalhando com Login

--- ram/src/main/java/com/jmc/mazebank/App.java
+++ ram/src/main/java/com/jmc/mazebank/App.java
@@ -4,6 +4,29 @@
 
 package com.jmc.mazebank;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+public class App extends Application {
+
+    @SuppressWarnings("exports")
+    @Override
+    public void start(Stage stage) throws Exception {
+        /*FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());*/
+
+        Parent root = FXMLLoader.load(getClass().getResource("/com/jmc/mazebank/Fxml/Login.fxml"));
+        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- ram/src/main/java/com/jmc/mazebank/Controllers/LoginController.java
+++ ram/src/main/java/com/jmc/mazebank/Controllers/LoginController.java
@@ -4,6 +4,36 @@
 
 package com.jmc.mazebank.Controllers;
 
-public class LoginController {
-    
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController implements  Initializable{
+    @FXML
+    private ChoiceBox<?> acc_selector;
+
+    @FXML
+    private Label error_lbl;
+
+    @FXML
+    private Button login_btn;
+
+    @FXML
+    private TextField password_fld;
+
+    @FXML
+    private TextField payee_adress_fld;
+
+    @FXML
+    private Label payee_adress_lbl;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 }
