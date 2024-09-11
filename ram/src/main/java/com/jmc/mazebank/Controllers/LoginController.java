6c79630e2c5d700909edf329536e76c3f00f622c
RamadanismaeL <ramadan.ismael02@gmail.com>
2024-09-11 19:17:20 +0200

    commit

--- ram/src/main/java/com/jmc/mazebank/Controllers/LoginController.java
+++ ram/src/main/java/com/jmc/mazebank/Controllers/LoginController.java
@@ -4,6 +4,37 @@
 
 package com.jmc.mazebank.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
-    
+
+    @FXML
+    private ChoiceBox<?> acc_selector;
+
+    @FXML
+    private Button close_btn;
+
+    @FXML
+    private Label error_lbl;
+
+    @FXML
+    private Button login_btn;
+
+    @FXML
+    private TextField password_fld;
+
+    @FXML
+    private TextField payee_address_fld;
+
+    @FXML
+    private Label payee_address_lbl;    
+
+    @FXML
+    private void handleButtonClick() {
+        System.out.println("Button cliecked!");
+    }
 }
