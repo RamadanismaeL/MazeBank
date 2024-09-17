368fb6ede63e1278b65cfdf4eedc6a2d17f9827a
RamadanismaeL <ramadan.ismael02@gmail.com>
2024-09-17 15:39:20 +0200

    logincontroller commited

--- ram/src/main/java/com/jmc/mazebank/Controllers/LoginController.java
+++ ram/src/main/java/com/jmc/mazebank/Controllers/LoginController.java
@@ -2,38 +2,15 @@
  * @author Ramadan ismaeL
  */
 
-package com.jmc.mazebank.Controllers;
+ package com.jmc.mazebank.Controllers;
 
+ import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
-public class LoginController {
-    @FXML
-    private ChoiceBox<?> acc_selector;
-
-    @FXML
-    private Button close_btn;
-
-    @FXML
-    private Label error_lbl;
-
-    @FXML
-    private Button login_btn;
-
-    @FXML
-    private TextField password_fld;
-
-    @FXML
-    private TextField payee_address_fld;
-
-    @FXML
-    private Label payee_address_lbl;
-
-    @FXML
-    private void close() {
-        System.exit(0);
-    }
-}
+ 
+ public class LoginController { 
+     @FXML
+     public void close() throws IOException {
+         System.exit(0);
+     }
+ }
+ 
